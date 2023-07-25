19d58fec8b81fdad1bf267bf7f330e08e83efa17
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-25 12:29:36 +0900

    add: 04_나이차이 cpp problem solve

---
+++ Algorithm-CPP/04_나이차이.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num;
+
+    cin >> n;
+
+    int ages[n];
+
+    for(int i=0; i<n; i++) {
+        cin >> num;
+        ages[i] = num; 
+    }
+
+    cout << *max_element(ages, ages + n) - *min_element(ages, ages + n) << endl;
+
+    return 0;
+}
