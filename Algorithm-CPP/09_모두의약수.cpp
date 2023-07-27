d2704a24aee2210ca0cbe9ee954bbd2e16ec9135
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-27 15:50:56 +0900

    chore: change j increase size

--- Algorithm-CPP/09_모두의약수.cpp
+++ Algorithm-CPP/09_모두의약수.cpp
@@ -5,21 +5,21 @@ int main() {
     int num;
     scanf("%d", &num);
 
-    int count[num];
+    int count[num+1];
 
-    for(int i = 0; i<num; i++) {
+    for(int i = 0; i<num+1; i++) {
         count[i] = 0;
     }
 
-    for(int i = 0; i<num; i++) {
-        for (int j = i; j<num; j++) {
-            if((j + 1) % (i + 1) == 0) {
+    for(int i = 1; i<num+1; i++) {
+        for (int j = i; j<num+1; j=j+i) {
+            if(j % i == 0) {
                 count[j]++;
             }
         }
     }
 
-    for(int i = 0; i<num; i++) {
+    for(int i = 1; i<num+1; i++) {
         printf("%d ", count[i]);
     }
 
