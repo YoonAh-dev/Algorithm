282ebd486603eba5e40619ccf657b918eb6801fe
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-27 14:38:40 +0900

    add: 08_올바른괄호 cpp problem solve

---
+++ Algorithm-CPP/08_올바른괄호.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string str;
+    stack<char> stack;
+
+    cin >> str;
+
+    for(int i = 0; i<str.length(); i++) {
+        if(str[i] == '(') {
+            stack.push(str[i]);
+        } else {
+            if(stack.top() == '(') {
+                stack.pop();
+            }
+        }
+    }
+
+    if(stack.empty()) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
