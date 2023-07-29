7366723d5425e2a8f01c920f78d303117d61f45c
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-29 11:33:12 +0900

    chore: change int to string

--- Algorithm-CPP/13_가장많이사용된자릿수.cpp
+++ Algorithm-CPP/13_가장많이사용된자릿수.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    int n, nums[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int nums[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    string n;
 
     cin >> n;
 
-    while(n != 0) {
-        int index = n % 10;
-        n /= 10;
+    for(int i=0; n[i] != '\0'; i++) {
+        int index = n[i] - '0';
         nums[index]++;
     }
-
+    
     int max = nums[0];
-    int result;
+    int result = 0;
 
     for(int i = 1; i<10; i++) {
         if(nums[i] > max) {
