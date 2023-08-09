bd7c98d66a106aedc0a98ded26c133df00d9a502
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-09 11:03:02 +0900

    chore: change char array

--- Algorithm-CPP/31_탄화수소질량.cpp
+++ Algorithm-CPP/31_탄화수소질량.cpp
@@ -3,23 +3,31 @@
 using namespace std;
 
 int main() {
-    int res = 0;
-    string str;
+    int index = 0;
+    string str, s[4];
+
     cin >> str;
 
-    for(int i = 0; str[i] != '\0'; ) {
-        if(isdigit(str[i + 1]) != 0) {
-            if (str[i] == 'C') res += 12 * (str[i + 1] - '0');
-            else if (str[i] == 'H') res += 1 * (str[i + 1] - '0');
-            i += 2;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(isdigit(str[i])) {
+            string temp = "";
+            int cnt = 0;
+            for(int j = i; isdigit(str[j]) != 0; j++) {
+                temp += str[j];
+                cnt++;
+            }
+            i += cnt - 1;
+            s[index++] = temp;
         } else {
-            if (str[i] == 'C') res += 12;
-            else if (str[i] == 'H') res += 1;
-            i++;
+            s[index++] = str[i];
+        }
+
+        if(isdigit(str[i]) == 0 && isdigit(str[i + 1]) == 0) {
+            s[index++] = "1";
         }
     }
 
-    cout << res << endl;
+    cout << stoi(s[1]) * 12 + stoi(s[3]) * 1 << endl;
 
     return 0;
 }
