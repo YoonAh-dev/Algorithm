813899d687aca8ad02a6dfcfbe03e2ecaa5bd9e2
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-17 10:00:45 +0900

    del: delete print

--- Algorithm-CPP/40_교집합.cpp
+++ Algorithm-CPP/40_교집합.cpp
@@ -33,12 +33,6 @@ int main() {
         }
     }
 
-    for(int i = 0; i < n; i++) cout << arr1[i] << " ";
-    cout << endl;
-
-    for(int i = 0; i < m; i++) cout << arr2[i] << " ";
-    cout << endl;
-
     int i = 0, j = 0;
     vector<int> res;
     while(i != n && j != m) {
