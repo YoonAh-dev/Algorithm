24c074527f47983929ebe7966a77eb6312017060
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-17 09:58:21 +0900

    add: 40_교집합 cpp problem solve

---
+++ Algorithm-CPP/40_교집합.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr1[n];
+    for(int i = 0; i < n; i++) cin >> arr1[i];
+    for(int i = 1; i < n; i++) {
+        for(int j = i; j > 0; j--) {
+            if(arr1[j - 1] < arr1[j]) break;
+
+            int temp = arr1[j];
+            arr1[j] = arr1[j - 1];
+            arr1[j - 1] = temp;
+        }
+    }
+
+    int m;
+    cin >> m;
+
+    int arr2[m];
+    for(int i = 0; i < m; i++) cin >> arr2[i];
+    for(int i = 1; i < m; i++) {
+        for(int j = i; j > 0; j--) {
+            if(arr2[j - 1] < arr2[j]) break;
+
+            int temp = arr2[j];
+            arr2[j] = arr2[j - 1];
+            arr2[j - 1] = temp;
+        }
+    }
+
+    for(int i = 0; i < n; i++) cout << arr1[i] << " ";
+    cout << endl;
+
+    for(int i = 0; i < m; i++) cout << arr2[i] << " ";
+    cout << endl;
+
+    int i = 0, j = 0;
+    vector<int> res;
+    while(i != n && j != m) {
+        if(arr1[i] == arr2[j]) {
+            res.push_back(arr1[i]);
+            i++; j++;
+        } 
+        else if(arr1[i] > arr2[j]) j++;
+        else if(arr1[i] < arr2[j]) i++;
+    }
+
+    for(int i = 0; i < res.size(); i++) cout << res[i] << " ";
+    
+    return 0;
+}
