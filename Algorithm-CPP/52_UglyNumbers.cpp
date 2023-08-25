3920cc62b8b245eca3da69d5661bbc3095151e87
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-25 11:45:22 +0900

    add: 52_UglyNumbers cpp problem solve

---
+++ Algorithm-CPP/52_UglyNumbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1, res = 1;
+    cin >> n;
+
+    while(cnt < n) {
+        res++;
+        int temp = res;
+        while(true) {
+            for(int i = 2; temp > 1; ) {
+                if(i > 5 && temp > 1) break;
+
+                if(temp % i == 0) {
+                    temp /= i;
+                } else i++;
+            }
+
+            if(temp <= 1) cnt++;
+            break;
+        }
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
