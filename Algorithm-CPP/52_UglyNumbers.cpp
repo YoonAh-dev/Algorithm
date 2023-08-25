b9e1bf7c20e93aee8ed175aa384421a120ac99d1
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-25 12:35:49 +0900

    chore: change while loop code

--- Algorithm-CPP/52_UglyNumbers.cpp
+++ Algorithm-CPP/52_UglyNumbers.cpp
@@ -2,24 +2,33 @@
 using namespace std;
 
 int main() {
-    int n, cnt = 1, res = 1;
+    int n;
     cin >> n;
 
-    while(cnt < n) {
-        res++;
-        int temp = res;
-        for(int i = 2; temp > 1; ) {
-            if(i > 5 && temp > 1) break;
-
-            if(temp % i == 0) {
-                temp /= i;
-            } else i++;
+    int p2 = 1, p3 = 1, p5 = 1, index = 1;
+    int arr[1501] = {0, 1};
+    while(index < n) {
+        int p2V = arr[p2] * 2, p3V = arr[p3] * 3, p5V = arr[p5] * 5;
+        
+        if(p2V < p3V && p2V < p5V) arr[++index] = arr[p2++] * 2;
+        else if(p3V < p2V && p3V < p5V) arr[++index] = arr[p3++] * 3;
+        else if(p5V < p2V && p5V < p3V) arr[++index] = arr[p5++] * 5;
+        else if(p2V == p3V && p2V < p5V) {
+            arr[++index] = arr[p2] * 2;
+            p2++; p3++;
+        } else if(p2V == p5V && p2V < p3V) {
+            arr[++index] = arr[p2] * 2;
+            p2++; p5++;
+        } else if(p3V == p5V && p3V < p2V) {
+            arr[++index] = arr[p3] * 3;
+            p3++; p5++;
+        } else if(p2V == p3V && p2V == p5V) {
+            arr[++index] = arr[p2] * 2;
+            p2++; p3++; p5++;
         }
-
-        if(temp <= 1) cnt++;
     }
 
-    cout << res << endl;
+    cout << arr[n] << endl;
 
     return 0;
 }
