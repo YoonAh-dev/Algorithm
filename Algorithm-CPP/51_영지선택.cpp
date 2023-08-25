7db9e3407b5160ce000e57dcc89719152f3a7373
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-25 11:31:24 +0900

    chore: terr arr to global variable

--- Algorithm-CPP/51_영지선택.cpp
+++ Algorithm-CPP/51_영지선택.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int terr[702][702];
+
 int main() {
     freopen("input.txt", "rt", stdin);
 
     int H, W, h, w, res = 0, max = 0;
     cin >> H >> W;
 
-    int temp[H+1][W+1], terr[H+1][W+1];
-    for(int i = 0; i <= H; i++) {
-        for(int j = 0; j <= W; j++) {
-            temp[i][j] = 0;
-            terr[i][j] = 0;
-        }
-    }
-
     for(int i = 1; i <= H; i++) {
         for(int j = 1; j <= W; j++) {
-            cin >> temp[i][j];
-            terr[i][j] = terr[i - 1][j] + terr[i][j - 1] - terr[i - 1][j - 1] + temp[i][j];
+            int input;
+            cin >> input;
+            terr[i][j] = terr[i - 1][j] + terr[i][j - 1] - terr[i - 1][j - 1] + input;
         }
     }
 
