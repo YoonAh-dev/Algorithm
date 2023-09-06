9e3252d5c2f9b2e406f1c48af1e852ac4e853bf0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-09-06 10:50:09 +0900

    add: 71_송아지찾기 cpp problem solve

---
+++ Algorithm-CPP/71_송아지찾기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int d[] = {1, -1, 5};
+    queue<pair<int, int>> Q;
+    int S, E;
+    cin >> S >> E;
+
+    Q.push({5, 0});
+
+    while(!Q.empty()) {
+        pair<int, int> x = Q.front();
+        Q.pop();
+        for(int i = 0; i < 3; i++) {
+            if(x.first + d[i] == E) {
+                cout << x.second + 1 << endl;
+                return 0;
+            } else {
+                Q.push({x.first + d[i], x.second + 1});
+            }
+        }
+    }
+}
---
+++ Algorithm-Swift/Algorithm-Swift/Baekjoon/정렬/10989. 수 정렬하기 3.swift
@@ -0,0 +1,8 @@
+//
+//  10989. 수 정렬하기 3.swift
+//  Algorithm-Swift
+//
+//  Created by SHIN YOON AH on 2023/09/05.
+//
+
+import Foundation
