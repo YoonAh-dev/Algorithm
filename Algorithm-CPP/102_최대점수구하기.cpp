6388a87f531506a9694e4933ac0bdeaedbb3547d
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-12-01 17:23:55 +0900

    add: sort logic

--- Algorithm-CPP/102_최대점수구하기.cpp
+++ Algorithm-CPP/102_최대점수구하기.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 using namespace std;
 
+bool cmp(pair<int, int> a, pair<int, int> b) { 
+    if(a.second == b.second) return a.first < b.first;
+    else return a.second < b.second; 
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
 
@@ -11,18 +16,26 @@ int main() {
     int n, m, score, time;
     cin >> n >> m;
     vector<int> dy(m + 1);
+    vector<pair<int, int>> times;
 
     for(int i = 0; i < n; i++) {
         cin >> score >> time;
+        times.push_back({ score, time });
+    }
+
+    sort(times.begin(), times.end(), cmp);
+
+    for(int i = 0; i < times.size(); i++) {
+        int currentScore = times[i].first, currentTime = times[i].second;
 
         for(int j = m; j >= 0; j--) {
-            if(j == time) {
-                dy[j] = max(dy[j], score);
+            if(j == currentTime) {
+                dy[j] = max(dy[j], dy[j - currentTime] + currentScore);
                 continue;
             }
-
-            if(dy[j] > 0 && j + time <= m) {
-                dy[j + time] = max(dy[j + time], dy[j] + score);     
+            
+            if(dy[j] > 0 && j + currentTime <= m) {
+                dy[j + currentTime] = max(dy[j + currentTime], dy[j] + currentScore);     
             }
         }
     }
